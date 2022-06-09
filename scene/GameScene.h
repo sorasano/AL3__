284dbdd03cb62101df0ea280d72bcdf462c6bbc2
SoraSano <k021g1331@g.neec.ac.jp>
2022-06-09 15:07:10 +0900

    ワールド返還行列を作成

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,15 +1,16 @@
 ﻿#pragma once
 
 #include "Audio.h"
-#include "DirectXCommon.h"
+#include "DebugCamera.h"
 #include "DebugText.h"
+#include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "DebugCamera.h"
+#include <DirectXMath.h>
 
 /// <summary>
 /// ゲームシーン
@@ -48,97 +49,16 @@ class GameScene {
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
 
-	Vector3 vertex[8] = {
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
 
-	  {0.0f, 0.0f, 0.0f},
-      {0.0f, 5.0f, 0.0f},
-      {5.0f, 5.0f, 0.0f},
-      {5.0f, 0.0f, 0.0f},
-      {0.0f, 0.0f, 5.0f},
-      {0.0f, 5.0f, 5.0f},
-      {5.0f, 5.0f, 5.0f},
-      {5.0f, 0.0f, 5.0f}
-	};
+	// 3Dモデル
+	Model* model_ = nullptr;
 
-	int edgeList[12][2] = {
-	  {0, 1},
-      {1, 2},
-      {2, 3},
-      {3, 0},
-      {0, 4},
-      {1, 5},
-	  {2, 6},
-      {3, 7},
-      {4, 5},
-      {5, 6},
-      {6, 7},
-      {7, 4}
-    };
-
-
-	//アフィン変換
-
-	float moveBox[8][4] = {
-	  {0, 0, 0, 1},
-      {0, 5, 0, 1},
-      {5, 5, 0, 1},
-      {5, 0, 0, 1},
-	  {0, 0, 5, 1},
-      {0, 5, 5, 1},
-      {5, 5, 5, 1},
-      {5, 0, 5, 1}
-    };
-
-	float rotateBox[8][4] = {
-	  {0, 0, 0, 1},
-      {0, 5, 0, 1},
-      {5, 5, 0, 1},
-      {5, 0, 0, 1},
-	  {0, 0, 5, 1},
-      {0, 5, 5, 1},
-      {5, 5, 5, 1},
-      {5, 0, 5, 1}
-    };
-
-	float scareBox[8][4] = {
-	  {0, 0, 0, 1},
-      {0, 5, 0, 1},
-      {5, 5, 0, 1},
-      {5, 0, 0, 1},
-	  {0, 0, 5, 1},
-      {0, 5, 5, 1},
-      {5, 5, 5, 1},
-      {5, 0, 5, 1}
-    };
-	
-	float moveInitialBox[8][4] = {};
-
-	float rotateInitialBox[8][4] = {};
-
-	float scareInitialBox[8][4] = {};
-
-
-	//基準点を原点に移動する用変数
-
-	//左上の点[0][]の初期値
-	float moveInitialPosX = moveBox[0][0];
-	float moveInitialPosY = moveBox[0][1];
-	float moveInitialPosZ = moveBox[0][2];
-
-	//原点移動用
-	float startPointMove[4][4]{};
-
-	//初期値に戻す用
-	float initialPointMove[4][4]{};
-
-	//平行移動
-	float move[4][4]{};
-
-	//回転
-	float rotate[4][4]{};
-
-	//拡大縮小
-	float scare[4][4]{};
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
 
 	//デバックカメラ
 	DebugCamera* debugCamera_ = nullptr;
